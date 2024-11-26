90e06d907948f4caf61ffcbd11b43043564cf5d8
Thomas Hayes <thomasjbhayes@gmail.com>
2024-11-26 23:17:47 +0000

    GridVolt 233 - added further alarm states

--- io.openems.edge.ess.gridvolt.gridvolt233/src/io/openems/edge/ess/gridvolt/gridvolt233/GridVolt233.java
+++ io.openems.edge.ess.gridvolt.gridvolt233/src/io/openems/edge/ess/gridvolt/gridvolt233/GridVolt233.java
@@ -146,6 +146,146 @@ public interface GridVolt233
 		THYRISTOR_OPEN_CIRCUIT(Doc.of(Level.WARNING).text("Thyristor open circuit")),
 		TRANSFORMER_PRE_CHARGING_FAULT(Doc.of(Level.FAULT).text("Transformer pre-charging fault")),
 
+		// 1048
+		ABNORMAL_GRID_CURRENT_ZERO_OFFSET(Doc.of(Level.WARNING)),
+		ABNORMAL_DC_CURRENT_ZERO_OFFSET(Doc.of(Level.WARNING)),
+		ABNORMAL_LEAKAGE_CURRENT_ZERO_OFFSET(Doc.of(Level.WARNING)),
+		REFERENCE_ABNORMALITY_2_5_VOLT(Doc.of(Level.WARNING).text("2.5V Reference abnormality")),
+		
+		// 1049
+		HIGH_AMBIENT_TEMPERATURE(Doc.of(Level.WARNING).text("High ambient temperature")),
+		FAN_FAULT(Doc.of(Level.FAULT).text("Fan fault")),
+		GRID_OVERLOAD_WARNING(Doc.of(Level.WARNING).text("Grid overload warning")),
+		ABNORMAL_OIL_ENGINE_STARTUP(Doc.of(Level.WARNING).text("Abnormal oil engine startup")),
+		
+		// 1050
+		POSITIVE_BUSBAR_OVERVOLTAGE(Doc.of(Level.WARNING).text("Positive busbar over-voltage")),
+		NEGATIVE_BUSBAR_OVERVOLTAGE(Doc.of(Level.WARNING).text("Negative busbar over-voltage")),
+		DC_TOTAL_BUSBAR_OVERVOLTAGE(Doc.of(Level.WARNING).text("DC total busbar overvoltage")),
+		BUSBAR_HALF_VOLTAGE_IMBALANCE(Doc.of(Level.WARNING).text("Busbar half-voltage imbalance")),
+		DC_BUSBAR_SHORT_CIRCUIT(Doc.of(Level.WARNING).text("DC Busbar short circuit")),
+		DC_OVERCURRENT(Doc.of(Level.WARNING).text("DC Overcurrent")),
+		BALANCE_BRIDGE_OVERCURRENT(Doc.of(Level.WARNING).text("Balance bridge overcurrent")),
+		DC_VOLTAGE_REVERSE_CONDITION(Doc.of(Level.WARNING).text("DC Voltage reverse condition")),
+		LOW_DC_VOLTAGE(Doc.of(Level.WARNING).text("Low DC Voltage")),
+		HIGH_DC_VOLTAGE(Doc.of(Level.WARNING).text("High DC Voltage")),
+		ABNORMAL_INSULATION_IMPEDANCE(Doc.of(Level.WARNING).text("Abnormal insulation impedance")),
+		LOW_PHOTOVOLTAIC_POWER_SHUTDOWN(Doc.of(Level.WARNING).text("Low photovoltaic power shutdown")),
+		
+		// 1051
+		ABNORMAL_INVERTER_VOLTAGE_PHASE_A(Doc.of(Level.WARNING).text("Abnormal inverter voltage phase A")),
+		ABNORMAL_INVERTER_VOLTAGE_PHASE_B(Doc.of(Level.WARNING).text("Abnormal inverter voltage phase B")),
+		ABNORMAL_INVERTER_VOLTAGE_PHASE_C(Doc.of(Level.WARNING).text("Abnormal inverter voltage phase C")),
+		ABNORMAL_DC_VOLTAGE_IN_INVERTER_VOLTAGE_PHASE_A(Doc.of(Level.WARNING).text("Abnormal DC voltage in inverter phase A")),
+		ABNORMAL_DC_VOLTAGE_IN_INVERTER_VOLTAGE_PHASE_B(Doc.of(Level.WARNING).text("Abnormal DC voltage in inverter phase B")),
+		ABNORMAL_DC_VOLTAGE_IN_INVERTER_VOLTAGE_PHASE_C(Doc.of(Level.WARNING).text("Abnormal DC voltage in inverter phase C")),
+		PHASE_A_OUTPUT_OVERLOAD_SHUTDOWN(Doc.of(Level.WARNING).text("Phase A output overload shutdown")),
+		PHASE_B_OUTPUT_OVERLOAD_SHUTDOWN(Doc.of(Level.WARNING).text("Phase B output overload shutdown")),
+		PHASE_C_OUTPUT_OVERLOAD_SHUTDOWN(Doc.of(Level.WARNING).text("Phase C output overload shutdown")),
+		OUTPUT_OVERCURRENT_PHASE_A(Doc.of(Level.WARNING).text("Output overcurrent phase A")),
+		OUTPUT_OVERCURRENT_PHASE_B(Doc.of(Level.WARNING).text("Output overcurrent phase B")),
+		OUTPUT_OVERCURRENT_PHASE_C(Doc.of(Level.WARNING).text("Output overcurrent phase C")),
+		OUTPUT_SHORT_CIRCUIT_PHASE_A(Doc.of(Level.WARNING).text("Output short circuit phase A")),
+		OUTPUT_SHORT_CIRCUIT_PHASE_B(Doc.of(Level.WARNING).text("Output short circuit phase B")),
+		OUTPUT_SHORT_CIRCUIT_PHASE_C(Doc.of(Level.WARNING).text("Output short circuit phase C")),
+		INVERTER_PHASE_NOT_SYNCHRONISED(Doc.of(Level.WARNING).text("Inverter phase not synchronized")),
+		
+		// 1052
+		GRID_OVER_VOLTAGE(Doc.of(Level.WARNING).text("Grid over-voltage")),
+		GRID_UNDER_VOLTAGE(Doc.of(Level.WARNING).text("Grid under-voltage")),
+		GRID_OVER_FREQUENCY(Doc.of(Level.WARNING).text("Grid over-frequency")),
+		GRID_UNDER_FREQUENCY(Doc.of(Level.WARNING).text("Grid under-frequency")),
+		ISLANDING_PROTECTION(Doc.of(Level.WARNING).text("Islanding protection")),
+		GRID_PHASE_MISMATCH(Doc.of(Level.WARNING).text("Grid phase mismatch")),
+		AC_POWER_LOSS(Doc.of(Level.WARNING).text("AC Power Loss")),
+		AC_WAVE_BY_WAVE_POWER_LIMITING_SHUTDOWN(Doc.of(Level.WARNING).text("AC Wave-by-wave power limiting shutdown")),
+		PARALLEL_CABLING_FAULT(Doc.of(Level.WARNING).text("Parallel cabling fault")),
+		CARRIER_SYNCHRONIZATION_FAULT(Doc.of(Level.WARNING).text("Carrier synchronization fault")),
+		INVERTER_SYNCHONIZATION_FAULT(Doc.of(Level.WARNING).text("Inverter synchonization fault")),
+		PARALLEL_COMMUNICATION_FAULT(Doc.of(Level.WARNING).text("Parallel communication fault")),
+		AC_FUSE_FAULT(Doc.of(Level.WARNING).text("AC fuse fault")),
+		POWER_TRANSISTOR_OVER_TEMPERATURE(Doc.of(Level.WARNING).text("Power transistor over temperature")),
+		POWER_SUPPLY_FAULT_(Doc.of(Level.WARNING).text("Power supply fault")),
+		LEAKAGE_CURRENT_FAULT_(Doc.of(Level.WARNING).text("Leakage current fault")),
+		
+		// 1053
+		DC_PRECHARGE_FAULT(Doc.of(Level.WARNING).text("DC Precharge fault")),
+		AC_PRECHARGE_FAULT(Doc.of(Level.WARNING).text("AC Precharge fault")),
+		DC_RELAY_SHORT_CIRCUIT(Doc.of(Level.WARNING).text("DC relay short circuit")),
+		DC_RELAY_OPEN_CIRCUIT(Doc.of(Level.WARNING).text("DC relay open circuit")),
+		AC_RELAY_SHORT_CIRCUIT_PHASE_A(Doc.of(Level.WARNING).text("AC relay short circuit phase A")),
+		AC_RELAY_SHORT_CIRCUIT_PHASE_B(Doc.of(Level.WARNING).text("AC relay short circuit phase B")),
+		AC_RELAY_SHORT_CIRCUIT_PHASE_C(Doc.of(Level.WARNING).text("AC relay short circuit phase C")),
+		AC_RELAY_OPEN_CIRCUIT_PHASE_A(Doc.of(Level.WARNING).text("AC relay open circuit phase A")),
+		AC_RELAY_OPEN_CIRCUIT_PHASE_B(Doc.of(Level.WARNING).text("AC relay open circuit phase B")),
+		AC_RELAY_OPEN_CIRCUIT_PHASE_C(Doc.of(Level.WARNING).text("AC relay open circuit phase C")),
+		BRIDGE_ARM_SHOOT_THROUGH_PHASE_A(Doc.of(Level.WARNING).text("Bridge arm shoot through phase A")),
+		BRIDGE_ARM_SHOOT_THROUGH_PHASE_B(Doc.of(Level.WARNING).text("Bridge arm shoot through phase B")),
+		BRIDGE_ARM_SHOOT_THROUGH_PHASE_C(Doc.of(Level.WARNING).text("Bridge arm shoot through phase C")),
+		
+		// 1054
+		GRID_CURRENT_ZERO_OFFSET_ABNORMALITY(Doc.of(Level.WARNING).text("Grid current zero offset abnormality")),
+		INVERTER_CURRENT_ZERO_OFFSET_ABNORMALITY(Doc.of(Level.WARNING).text("Inverter current zero offset abnormality")),
+		INVERTER_CURRENT_DC_COMPONENT_ZERO_OFFSET_ABNORMALITY(Doc.of(Level.WARNING).text("Inverter current DC component zero offset abnormality")),
+		DC_CURRENT_ZERO_OFFSET_ABNORMALITY(Doc.of(Level.WARNING).text("DC current zero offset abnormality")),
+		BALANCE_BRIDGE_CURRENT_ZERO_OFFSET_ABNORMALITY(Doc.of(Level.WARNING).text("Balance bridge current zero offset abnormality")),
+		LEAKAGE_CURRENT_ZERO_OFFSET_ABNORMALITY(Doc.of(Level.WARNING).text("Leakage current zero offset abnormality")),
+		_2_5V_CURRENT_ZERO_OFFSET_ABNORMALITY(Doc.of(Level.WARNING).text("2.5V current zero offset abnormality")),
+		
+		// 1055
+		OUTPUT_OVERLOAD_ALARM_PHASE_A(Doc.of(Level.WARNING).text("Output overload alarm Phase A")),
+		OUTPUT_OVERLOAD_ALARM_PHASE_B(Doc.of(Level.WARNING).text("Output overload alarm Phase B")),
+		OUTPUT_OVERLOAD_ALARM_PHASE_C(Doc.of(Level.WARNING).text("Output overload alarm Phase C")),
+		LOW_VOLTAGE_RIDE_THROUGH(Doc.of(Level.WARNING).text("Low voltage ride through")),
+		HIGH_VOLTAGE_RIDE_THROUGH(Doc.of(Level.WARNING).text("High voltage ride through")),
+		BALANCE_BRIDGE_WAVE_BY_WAVE_CURRENT_LIMITING_ALARM(Doc.of(Level.WARNING).text("Balance bridge wave-by-wave current limiting alarm")),
+		BALANCE_BRIDGE_OVER_TEMPERATURE(Doc.of(Level.WARNING).text("Balance bridge over temperature")),
+		_HIGH_AMBIENT_TEMPERATURE(Doc.of(Level.WARNING).text("High ambient temperature")),
+		TEMPERATURE_DERATING(Doc.of(Level.WARNING).text("Temperature derating")),
+		DC_LIGHTNING_PROTECTION_FAULT(Doc.of(Level.WARNING).text("DC Lightning protection fault")),
+		AC_LIGHTNING_PROTECTION_FAULT(Doc.of(Level.WARNING).text("AC Lightning protection fault")),
+		FAN_FAULT_1(Doc.of(Level.WARNING).text("Fan fault 1")),
+		FAN_FAULT_2(Doc.of(Level.WARNING).text("Fan fault 2")),
+		BATTERY_OVERCHARGE(Doc.of(Level.WARNING).text("Battery overcharge")),
+		BATTERY_OVERDISCHARGE(Doc.of(Level.WARNING).text("Battery over-discharge")),
+		
+		// 1056
+		INTERNAL_COMMUNICATION_FAULT(Doc.of(Level.FAULT).text("Internal communication fault")),
+		
+		// 1220
+		POSITIVE_HV_BUS_OVERVOLTAGE(Doc.of(Level.FAULT).text("Positive HV bus overvoltage")),
+		NEGATIVE_HV_BUS_OVERVOLTAGE(Doc.of(Level.FAULT).text("Negative HV bus overvoltage")),
+		HV_DC_BUS_OVERVOLTAGE(Doc.of(Level.FAULT).text("HV DC Bus overvoltage")),
+		HV_BUS_UNBALANCED_HALF_VOLTAGE(Doc.of(Level.FAULT).text("HV bus unbalanced half voltage")),
+		HV_DC_BUS_SHORT_CIRCUIT(Doc.of(Level.FAULT).text("HV DC Bus short circuit")),
+		HV_SIDE_OVERCURRENT(Doc.of(Level.FAULT).text("HV side overcurrent")),
+		HV_SIDE_LOW_EXTERNAL_VOLTAGE(Doc.of(Level.FAULT).text("HV side low external voltage")),
+		HV_SIDE_HIGH_EXTERNAL_VOLTAGE(Doc.of(Level.FAULT).text("HV side high external voltage")),
+		HV_SIDE_OVERLOAD(Doc.of(Level.FAULT).text("HV side overload")),
+		GRADUAL_CURRENT_LIMITING_SHUTDOWN(Doc.of(Level.FAULT).text("Gradual current limiting shutdown")),
+		LOW_PHOTOVOLTAIC_POWER_SHUTDOWN_(Doc.of(Level.FAULT).text("Low photovoltaic power shutdown")),
+		
+		// 1221
+		EXTERNAL_VOLTAGE_REVERSE_CONNECTION_LV_SIDE_1(Doc.of(Level.WARNING).text("External voltage reverse connection LV side 1")),
+		LOW_EXTERNAL_VOLTAGE_LV_SIDE_1(Doc.of(Level.WARNING).text("Low external voltage LV side 1")),
+		HIGH_EXTERNAL_VOLTAGE_LV_SIDE_1(Doc.of(Level.WARNING).text("High external voltage LV side 1")),
+		ABNORMAL_INSULATION_RESISTANCE_LV_SIDE_1(Doc.of(Level.WARNING).text("High external voltage LV side 1")),
+		INVERNAL_OVERVOLTAGE_LV_SIDE_1(Doc.of(Level.WARNING).text("Abnormal insulation resistance LV side 1")),
+		SHORT_CIRCUIT_LV_SIDE_1(Doc.of(Level.WARNING).text("Short circuit LV side 1")),
+		OVER_CURRENT_LV_SIDE_1(Doc.of(Level.WARNING).text("Over current LV side 1")),
+		OVERLOAD_LV_SIDE_1(Doc.of(Level.WARNING).text("Overload LV side 1")),
+		EXTERNAL_VOLTAGE_REVERSE_CONNECTION_LV_SIDE_2(Doc.of(Level.WARNING).text("External voltage reverse connection LV side 2")),
+		LOW_EXTERNAL_VOLTAGE_LV_SIDE_2(Doc.of(Level.WARNING).text("Low external voltage LV side 2")),
+		HIGH_EXTERNAL_VOLTAGE_LV_SIDE_2(Doc.of(Level.WARNING).text("High external voltage LV side 2")),
+		ABNORMAL_INSULATION_RESISTANCE_LV_SIDE_2(Doc.of(Level.WARNING).text("High external voltage LV side 2")),
+		INVERNAL_OVERVOLTAGE_LV_SIDE_2(Doc.of(Level.WARNING).text("Abnormal insulation resistance LV side 2")),
+		SHORT_CIRCUIT_LV_SIDE_2(Doc.of(Level.WARNING).text("Short circuit LV side 2")),
+		OVER_CURRENT_LV_SIDE_2(Doc.of(Level.WARNING).text("Over current LV side 2")),
+		OVERLOAD_LV_SIDE_2(Doc.of(Level.WARNING).text("Overload LV side 2")),
+		
+		// 1222
+		INCORRECT_BATTERY_TYPE_CONFIGURATION(Doc.of(Level.WARNING).text("Incorrect battery type configuration")),
+		
 		
 		// 9.5.1 System statistics
 		BATTERY_MAXIMUM_DISCHARGE_CURRENT(Doc.of(OpenemsType.INTEGER)
--- io.openems.edge.ess.gridvolt.gridvolt233/src/io/openems/edge/ess/gridvolt/gridvolt233/GridVolt233Impl.java
+++ io.openems.edge.ess.gridvolt.gridvolt233/src/io/openems/edge/ess/gridvolt/gridvolt233/GridVolt233Impl.java
@@ -104,13 +104,154 @@ public class GridVolt233Impl extends AbstractOpenemsModbusComponent
 								.bit(9, GridVolt233.ChannelId.GRID_OVERLOAD_FAULT)
 								.bit(10, GridVolt233.ChannelId.BATTERY_FAULT)
 								),
-						m(new BitsWordElement(1046, this) // 
+						m(new BitsWordElement(1047, this) // 
 								.bit(0, GridVolt233.ChannelId.THYRISTOR_SHORT_CIRCUIT)
 								.bit(1, GridVolt233.ChannelId.THYRISTOR_OPEN_CIRCUIT)
 								.bit(2, GridVolt233.ChannelId.TRANSFORMER_PRE_CHARGING_FAULT)
+								),
+						m(new BitsWordElement(1048, this) // 
+								.bit(0, GridVolt233.ChannelId.ABNORMAL_GRID_CURRENT_ZERO_OFFSET)
+								.bit(1, GridVolt233.ChannelId.ABNORMAL_DC_CURRENT_ZERO_OFFSET)
+								.bit(2, GridVolt233.ChannelId.ABNORMAL_LEAKAGE_CURRENT_ZERO_OFFSET)
+								.bit(3, GridVolt233.ChannelId.REFERENCE_ABNORMALITY_2_5_VOLT)
+								),
+						m(new BitsWordElement(1049, this) // 
+								.bit(0, GridVolt233.ChannelId.HIGH_AMBIENT_TEMPERATURE)
+								.bit(1, GridVolt233.ChannelId.FAN_FAULT)
+								.bit(2, GridVolt233.ChannelId.GRID_OVERLOAD_WARNING)
+								.bit(3, GridVolt233.ChannelId.ABNORMAL_OIL_ENGINE_STARTUP)
+								),
+						m(new BitsWordElement(1050, this) // 
+								.bit(0, GridVolt233.ChannelId.POSITIVE_BUSBAR_OVERVOLTAGE)
+								.bit(1, GridVolt233.ChannelId.NEGATIVE_BUSBAR_OVERVOLTAGE)
+								.bit(2, GridVolt233.ChannelId.DC_TOTAL_BUSBAR_OVERVOLTAGE)
+								.bit(3, GridVolt233.ChannelId.BUSBAR_HALF_VOLTAGE_IMBALANCE)
+								.bit(4, GridVolt233.ChannelId.DC_BUSBAR_SHORT_CIRCUIT)
+								.bit(5, GridVolt233.ChannelId.DC_OVERCURRENT)
+								.bit(6, GridVolt233.ChannelId.BALANCE_BRIDGE_OVERCURRENT)
+								.bit(7, GridVolt233.ChannelId.DC_VOLTAGE_REVERSE_CONDITION)
+								.bit(8, GridVolt233.ChannelId.LOW_DC_VOLTAGE)
+								.bit(9, GridVolt233.ChannelId.HIGH_DC_VOLTAGE)
+								.bit(10, GridVolt233.ChannelId.ABNORMAL_INSULATION_IMPEDANCE)
+								.bit(11, GridVolt233.ChannelId.LOW_PHOTOVOLTAIC_POWER_SHUTDOWN)
+								),
+						m(new BitsWordElement(1051, this) // 
+								.bit(0, GridVolt233.ChannelId.ABNORMAL_INVERTER_VOLTAGE_PHASE_A)
+								.bit(1, GridVolt233.ChannelId.ABNORMAL_INVERTER_VOLTAGE_PHASE_B)
+								.bit(2, GridVolt233.ChannelId.ABNORMAL_INVERTER_VOLTAGE_PHASE_C)
+								.bit(3, GridVolt233.ChannelId.ABNORMAL_DC_VOLTAGE_IN_INVERTER_VOLTAGE_PHASE_A)
+								.bit(4, GridVolt233.ChannelId.ABNORMAL_DC_VOLTAGE_IN_INVERTER_VOLTAGE_PHASE_B)
+								.bit(5, GridVolt233.ChannelId.ABNORMAL_DC_VOLTAGE_IN_INVERTER_VOLTAGE_PHASE_C)
+								.bit(6, GridVolt233.ChannelId.PHASE_A_OUTPUT_OVERLOAD_SHUTDOWN)
+								.bit(7, GridVolt233.ChannelId.PHASE_B_OUTPUT_OVERLOAD_SHUTDOWN)
+								.bit(8, GridVolt233.ChannelId.PHASE_C_OUTPUT_OVERLOAD_SHUTDOWN)
+								.bit(9, GridVolt233.ChannelId.OUTPUT_OVERCURRENT_PHASE_A)
+								.bit(10, GridVolt233.ChannelId.OUTPUT_OVERCURRENT_PHASE_B)
+								.bit(11, GridVolt233.ChannelId.OUTPUT_OVERCURRENT_PHASE_C)
+								.bit(12, GridVolt233.ChannelId.OUTPUT_SHORT_CIRCUIT_PHASE_A)
+								.bit(13, GridVolt233.ChannelId.OUTPUT_SHORT_CIRCUIT_PHASE_B)
+								.bit(14, GridVolt233.ChannelId.OUTPUT_SHORT_CIRCUIT_PHASE_C)
+								.bit(15, GridVolt233.ChannelId.INVERTER_PHASE_NOT_SYNCHRONISED)
+								),
+						m(new BitsWordElement(1052, this) // 
+								.bit(0, GridVolt233.ChannelId.GRID_OVER_VOLTAGE)
+								.bit(1, GridVolt233.ChannelId.GRID_UNDER_VOLTAGE)
+								.bit(2, GridVolt233.ChannelId.GRID_OVER_FREQUENCY)
+								.bit(3, GridVolt233.ChannelId.GRID_UNDER_FREQUENCY)
+								.bit(4, GridVolt233.ChannelId.ISLANDING_PROTECTION)
+								.bit(5, GridVolt233.ChannelId.GRID_PHASE_MISMATCH)
+								.bit(6, GridVolt233.ChannelId.AC_POWER_LOSS)
+								.bit(7, GridVolt233.ChannelId.AC_WAVE_BY_WAVE_POWER_LIMITING_SHUTDOWN)
+								.bit(8, GridVolt233.ChannelId.PARALLEL_CABLING_FAULT)
+								.bit(9, GridVolt233.ChannelId.CARRIER_SYNCHONIZATION_FAULT)
+								.bit(10, GridVolt233.ChannelId.INVERTER_SYNCHONIZATION_FAULT)
+								.bit(11, GridVolt233.ChannelId.PARALLEL_COMMUNICATION_FAULT)
+								.bit(12, GridVolt233.ChannelId.AC_FUSE_FAULT)
+								.bit(13, GridVolt233.ChannelId.POWER_TRANSISTOR_OVER_TEMPERATURE)
+								.bit(14, GridVolt233.ChannelId.POWER_SUPPLY_FAULT_)
+								.bit(15, GridVolt233.ChannelId.LEAKAGE_CURRENT_FAULT_)
+								),
+						m(new BitsWordElement(1053, this) // 
+								.bit(0, GridVolt233.ChannelId.DC_PRECHARGE_FAULT)
+								.bit(1, GridVolt233.ChannelId.AC_PRECHARGE_FAULT)
+								.bit(2, GridVolt233.ChannelId.DC_RELAY_SHORT_CIRCUIT)
+								.bit(3, GridVolt233.ChannelId.DC_RELAY_OPEN_CIRCUIT)
+								.bit(4, GridVolt233.ChannelId.AC_RELAY_SHORT_CIRCUIT_PHASE_A)
+								.bit(5, GridVolt233.ChannelId.AC_RELAY_SHORT_CIRCUIT_PHASE_B)
+								.bit(6, GridVolt233.ChannelId.AC_RELAY_SHORT_CIRCUIT_PHASE_C)
+								.bit(7, GridVolt233.ChannelId.AC_RELAY_OPEN_CIRCUIT_PHASE_A)
+								.bit(8, GridVolt233.ChannelId.AC_RELAY_OPEN_CIRCUIT_PHASE_B)
+								.bit(9, GridVolt233.ChannelId.AC_RELAY_OPEN_CIRCUIT_PHASE_C)
+								.bit(10, GridVolt233.ChannelId.BRIDGE_ARM_SHOOT_THROUGH_PHASE_A)
+								.bit(11, GridVolt233.ChannelId.BRIDGE_ARM_SHOOT_THROUGH_PHASE_B)
+								.bit(12, GridVolt233.ChannelId.BRIDGE_ARM_SHOOT_THROUGH_PHASE_C)
+								),
+						m(new BitsWordElement(1054, this) // 
+								.bit(0, GridVolt233.ChannelId.GRID_CURRENT_ZERO_OFFSET_ABNORMALITY)
+								.bit(1, GridVolt233.ChannelId.INVERTER_CURRENT_ZERO_OFFSET_ABNORMALITY)
+								.bit(2, GridVolt233.ChannelId.INVERTER_CURRENT_DC_COMPONENT_ZERO_OFFSET_ABNORMALITY)
+								.bit(3, GridVolt233.ChannelId.DC_CURRENT_ZERO_OFFSET_ABNORMALITY)
+								.bit(4, GridVolt233.ChannelId.BALANCE_BRIDGE_CURRENT_ZERO_OFFSET_ABNORMALITY)
+								.bit(5, GridVolt233.ChannelId.LEAKAGE_CURRENT_ZERO_OFFSET_ABNORMALITY)
+								.bit(6, GridVolt233.ChannelId._2_5V_CURRENT_ZERO_OFFSET_ABNORMALITY)
+								),
+						m(new BitsWordElement(1055, this) // 
+								.bit(0, GridVolt233.ChannelId.OUTPUT_OVERLOAD_ALARM_PHASE_A)
+								.bit(1, GridVolt233.ChannelId.OUTPUT_OVERLOAD_ALARM_PHASE_B)
+								.bit(2, GridVolt233.ChannelId.OUTPUT_OVERLOAD_ALARM_PHASE_C)
+								.bit(3, GridVolt233.ChannelId.LOW_VOLTAGE_RIDE_THROUGH)
+								.bit(4, GridVolt233.ChannelId.HIGH_VOLTAGE_RIDE_THROUGH)
+								.bit(5, GridVolt233.ChannelId.BALANCE_BRIDGE_WAVE_BY_WAVE_CURRENT_LIMITING_ALARM)
+								.bit(6, GridVolt233.ChannelId.BALANCE_BRIDGE_OVER_TEMPERATURE)
+								.bit(7, GridVolt233.ChannelId._HIGH_AMBIENT_TEMPERATURE)
+								.bit(8, GridVolt233.ChannelId.TEMPERATURE_DERATING)
+								.bit(9, GridVolt233.ChannelId.DC_LIGHTNING_PROTECTION_FAULT)
+								.bit(10, GridVolt233.ChannelId.AC_LIGHTNING_PROTECTION_FAULT)
+								.bit(11, GridVolt233.ChannelId.FAN_FAULT_1)
+								.bit(12, GridVolt233.ChannelId.FAN_FAULT_2)
+								.bit(13, GridVolt233.ChannelId.BATTERY_OVERCHARGE)
+								.bit(14, GridVolt233.ChannelId.BATTERY_OVERDISCHARGE)
+								),
+						m(new BitsWordElement(1056, this) // 
+								.bit(0, GridVolt233.ChannelId.INTERNAL_COMMUNICATION_FAULT)
 								)
 						),
-				
+				new FC3ReadRegistersTask(1220, Priority.LOW, //
+						m(new BitsWordElement(1220, this) // 
+								.bit(0, GridVolt233.ChannelId.POSITIVE_HV_BUS_OVERVOLTAGE)
+								.bit(1, GridVolt233.ChannelId.NEGATIVE_HV_BUS_OVERVOLTAGE)
+								.bit(2, GridVolt233.ChannelId.HV_DC_BUS_OVERVOLTAGE)
+								.bit(3, GridVolt233.ChannelId.HV_BUS_UNBALANCED_HALF_VOLTAGE)
+								.bit(4, GridVolt233.ChannelId.HV_DC_BUS_SHORT_CIRCUIT)
+								.bit(5, GridVolt233.ChannelId.HV_SIDE_OVERCURRENT)
+								.bit(6, GridVolt233.ChannelId.HV_SIDE_LOW_EXTERNAL_VOLTAGE)
+								.bit(7, GridVolt233.ChannelId.HV_SIDE_HIGH_EXTERNAL_VOLTAGE)
+								.bit(8, GridVolt233.ChannelId.HV_SIDE_OVERLOAD)
+								.bit(9, GridVolt233.ChannelId.GRADUAL_CURRENT_LIMITING_SHUTDOWN)
+								.bit(10, GridVolt233.ChannelId.LOW_PHOTOVOLTAIC_POWER_SHUTDOWN_)
+								),
+						m(new BitsWordElement(1221, this) // 
+								.bit(0, GridVolt233.ChannelId.EXTERNAL_VOLTAGE_REVERSE_CONNECTION_LV_SIDE_1)
+								.bit(1, GridVolt233.ChannelId.LOW_EXTERNAL_VOLTAGE_LV_SIDE_1)
+								.bit(2, GridVolt233.ChannelId.HIGH_EXTERNAL_VOLTAGE_LV_SIDE_1)
+								.bit(3, GridVolt233.ChannelId.ABNORMAL_INSULATION_RESISTANCE_LV_SIDE_1)
+								.bit(4, GridVolt233.ChannelId.INVERNAL_OVERVOLTAGE_LV_SIDE_1)
+								.bit(5, GridVolt233.ChannelId.SHORT_CIRCUIT_LV_SIDE_1)
+								.bit(6, GridVolt233.ChannelId.OVER_CURRENT_LV_SIDE_1)
+								.bit(7, GridVolt233.ChannelId.OVERLOAD_LV_SIDE_1)
+								.bit(8, GridVolt233.ChannelId.EXTERNAL_VOLTAGE_REVERSE_CONNECTION_LV_SIDE_2)
+								.bit(9, GridVolt233.ChannelId.LOW_EXTERNAL_VOLTAGE_LV_SIDE_2)
+								.bit(10, GridVolt233.ChannelId.HIGH_EXTERNAL_VOLTAGE_LV_SIDE_2)
+								.bit(11, GridVolt233.ChannelId.ABNORMAL_INSULATION_RESISTANCE_LV_SIDE_2)
+								.bit(12, GridVolt233.ChannelId.INVERNAL_OVERVOLTAGE_LV_SIDE_2)
+								.bit(13, GridVolt233.ChannelId.SHORT_CIRCUIT_LV_SIDE_2)
+								.bit(14, GridVolt233.ChannelId.OVER_CURRENT_LV_SIDE_2)
+								.bit(15, GridVolt233.ChannelId.OVERLOAD_LV_SIDE_2)
+								),
+						m(new BitsWordElement(1222, this) // 
+								.bit(5, GridVolt233.ChannelId.INCORRECT_BATTERY_TYPE_CONFIGURATION)
+								)
+						),
 				// Status information
 				new FC3ReadRegistersTask(1400, Priority.LOW, // 
 						
